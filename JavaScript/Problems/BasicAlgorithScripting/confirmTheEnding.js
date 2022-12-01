9cdeda787907e8744f62e5b11cbf4eb457c9ea80
RamiroBertran <ramirobertra@gmail.com>
2022-12-01 03:02:51 -0300

    solved confirm ending without using endWith() method

--- JavaScript/Problems/BasicAlgorithScripting/confirmTheEnding.js
+++ JavaScript/Problems/BasicAlgorithScripting/confirmTheEnding.js
@@ -8,26 +8,33 @@ function confirmEnding(str, target) {
   return str.endsWith(target);
 }
 */
-
-function confirmEnding(st, targ) {
-  let target = targ.split("").reverse();
-  let str = st.split("").reverse();
-  let newArr = [];
-  for (let i = 0; i < target.length; i++) {
-    for (let j = 0; j < str.length; j++) {
-      console.log(target[i]);
-      console.log(str[j]);
-      if (target[i] !== str[j]) {
-        return "No es igual";
-      }
+function confirmEnding(str, target) {
+  let arr = str.split("");
+  let arr_two = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    arr_two.unshift(arr[i]);
+    if (arr_two.join("") === target) {
+      return true;
     }
-    return "si es igual";
   }
+  return false;
+}
+console.log(confirmEnding("Congratulation", "on"));
+//console.log(confirmEnding("He has to give me a new name", "name"));
+
+function confimEndingTwo(str, target) {
+  let arr = str.toLowerCase().split("").reverse();
+  let reverse_target = target.toLowerCase().split("").reverse().join("");
+  let container = [];
+  for (let i = 0; i < arr.length; i++) {
+    container.push(arr[i]);
+    if (container.join("") === reverse_target) {
+      return true;
+    }
+  }
+  return false;
 }
 
-//console.log(confirmEnding("Bastian", "n"));
-console.log(confirmEnding("Congratulation", "on"));
-//console.log(confirmEnding(
-//    "Walking on water and developing software from a specification are easy if both are frozen",
-//   "specification"));
-console.log(confirmEnding("He has to give me a new name", "name"));
+console.log(confimEndingTwo("Congratulation", "on"));
+console.log(confimEndingTwo("messi", "si"));
+console.log(confimEndingTwo("nickelodeon", "on"));
