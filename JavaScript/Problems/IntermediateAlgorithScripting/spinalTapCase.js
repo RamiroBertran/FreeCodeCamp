340d34ed983190a5813863791f676708467e1e15
RamiroBertran <ramirobertra@gmail.com>
2022-12-05 00:59:06 -0300

    spinalTapCase solved

--- JavaScript/Problems/IntermediateAlgorithScripting/spinalTapCase.js
+++ JavaScript/Problems/IntermediateAlgorithScripting/spinalTapCase.js
@@ -1,15 +1,53 @@
-/*
-Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
-
--1 should return this-is-spinal-tap
--2 this-is-spinal-tap
--3  the-andy-griffith-show
-*/
 function spinalCase(str) {
-  return str;
-}
+    let first; 
+    let segundo; 
+    let tercero; 
+    let cuarto  = []; 
+    let newarr; 
+    let sin; 
+    let arr = [];
 
-console.log(spinalCase("This Is Spinal Tap"));
+        if(/\s/g.test(str)){
+        first = str.replace(/-/, " ");
+        segundo = first.split(" "); 
+        for(let i = 0; i < segundo.length; i++){
+            segundo[i] = segundo[i][0].toLowerCase() + segundo[i].substring(1); 
+        }
+        tercero = segundo.join(" ").split(""); 
+
+        for(let i = 0; i < tercero.length;i++){
+            if(tercero[i] !== tercero[i].toUpperCase() || tercero[i] == " "){
+                cuarto.push(tercero[i])
+            } else if(tercero[i] == tercero[i].toUpperCase()){
+                cuarto.push(" "); 
+                cuarto.push(tercero[i]); 
+            }
+            
+        }
+            return cuarto.join("").replace(/\s/g, "-").toLowerCase(); 
+        }
+        else if(/_/g.test(str)){
+        newarr = str.replace(/_/g, "-"); 
+        return newarr.toLowerCase(); 
+        } else if(/\s/g.test(str) === false){
+            sin = str.split(""); 
+            for(let j = 0; j < sin.length; j++){
+                if(/[a-z]/g.test(sin[j])){
+                    arr.push(sin[j]);  
+                } else {
+                    arr.push("-"); 
+                    arr.push(sin[j]); 
+                }
+            }
+            return arr.join("").toLowerCase(); 
+
+        }
+
+    }
+
+console.log(spinalCase("AllThe-small Things")); 
+console.log(spinalCase("This Is Spinal Tap")); 
 console.log(spinalCase("thisIsSpinalTap"));
 console.log(spinalCase("The_Andy_Griffith_Show"));
-console.log(spinalCase("Teletubbies say Eh-oh"));
+console.log(spinalCase("Teletubbies say Eh-oh")); 
+
--- JavaScript/Problems/IntermediateAlgorithScripting/whereforeArtThou.js
+++ JavaScript/Problems/IntermediateAlgorithScripting/whereforeArtThou.js
@@ -1,56 +1,68 @@
-
 function whatIsInAName(collection, source) {
-let prop = Object.keys(source); 
-let newcol = [...collection]; 
-let container = []; 
-let arr = []; 
-    for(let i = 0; i < prop.length; i++){
-        container.push(newcol.filter(item => item[prop[i]] == source[prop[i]])); 
-    }
+  let prop = Object.keys(source);
+  let container = [];
+  let arr = [];
 
-    for(let i = 0; i < container.length;i++){
-        if(container[i].length  === 0){
-            return container[i]; 
+  for (let i = 0; i < prop.length; i++) {
+    container.push(
+      collection.filter((item) => item[prop[i]] == source[prop[i]])
+    );
+  }
+
+  for (let i = 0; i < container.length; i++) {
+    if (container[i].length === 0) {
+      return container[i];
+    }
+  }
+  arr = [...container[0]];
+  if (prop.length > 1) {
+    for (let i = 0; i < prop.length; i++) {
+      let j = 0;
+      while (arr.length > j) {
+        if (arr[j].hasOwnProperty(prop[i]) === false) {
+          arr.splice(j, 1);
+          j--;
         }
+        j++;
+      }
     }
-arr = [...container[0]]
-// creo un nuevo array sobre el que voy a iterar y al cortarlo, lo cual es necesario, no modificiar sobre el que estoy iterando. 
-console.log(newarr); 
-    if(prop.length > 1){
-    for(let i = 0; i < prop.length; i++){
-    console.log("Buscamos la prop: " + prop[i]); 
-    let j = 0; 
-        while(arr.length > j){
-            if(arr[j].hasOwnProperty(prop[i]) === false){
-                console.log(arr[j]); 
-                console.log(prop[i] + ": no esta"); 
-                arr.splice(j,1); 
-                j--; 
-                console.log(j); 
-                //ver el estado de newarr; 
-                
-            } else {
-                console.log(arr[j]); 
-                console.log(prop[i] + ": esta"); 
-        }
-        j++; 
-    }
-    }
-}
-return arr; 
+  }
+  return arr;
 }
 
+console.log(whatIsInAName([{ a: 1, b: 2, c: 3 }], { a: 1, b: 9999, c: 3 }));
 
-/* la tengo adentro: 
-console.log(whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3})); 
+console.log(
+  whatIsInAName(
+    [{ apple: 1, bat: 2 }, { apple: 1 }, { apple: 1, bat: 2, cookie: 2 }],
+    { apple: 1, cookie: 2 }
+  )
+);
 
-console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 })); 
- *
-console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })); 
+console.log(
+  whatIsInAName(
+    [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
+    { apple: 1, bat: 2 }
+  )
+);
 
-console.log( whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }));
+console.log(
+  whatIsInAName([{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }], {
+    apple: 1,
+  })
+);
 
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+console.log(
+  whatIsInAName(
+    [
+      { first: "Romeo", last: "Montague" },
+      { first: "Mercutio", last: null },
+      { first: "Tybalt", last: "Capulet" },
+    ],
+    { last: "Capulet" }
+  )
+);
 
-console.log(whatIsInAName([{"a": 1, "b": 2, "c": 3, "d": 9999}], {"a": 1, "b": 9999, "c": 3})); 
-*/
+console.log(
+  whatIsInAName([{ a: 1, b: 2, c: 3, d: 9999 }], { a: 1, b: 9999, c: 3 })
+);
