2275a1e9d386f85a6c42ff95425ae5c00c4cc405
RamiroBertran <ramirobertra@gmail.com>
2023-01-07 20:51:51 -0300

    steamroller solved

--- JavaScript/Problems/IntermediateAlgorithScripting/steamroller.js
+++ JavaScript/Problems/IntermediateAlgorithScripting/steamroller.js
@@ -1,42 +1,30 @@
 function steamrollArray(arr) {
-let newarr = []; 
-let jon; 
-let jon_two; 
-let test; 
-var toType = function(obj) {
-  return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
-}
-
-for(let i = 0; i < arr.length; i++){
-    if(toType({}) === toType(arr[i])){
-        newarr.push(arr[i]); 
-    } else if(toType([]) === toType(arr[i]) && arr[i].length > 0){
-        test = arr[i].join("").split(""); 
-        let regex = /\d/; 
-        for(let j = 0; j < test.length; j++){
-            if(regex.test(test[j])){
-            newarr.push(parseInt(test[j])); 
-            } else { 
-        newarr.push(test[j]);
-            }
-        }
-    } else if(toType([]) === toType(arr[i]) && arr[i].length == 0){ 
-    } else {
-        newarr.push(arr[i]); 
+  for(let i in arr){
+    if(Array.isArray(arr[i])){
+      arr[i] = arr[i].join("").split('');
+      if(arr[i].length < 1){
+        arr.splice(i,1);
+      }
+      if(arr[i].length == 1 && (/\d/).test(arr[i])){
+        arr.splice(i,1,parseFloat(arr[i][0]))
+      } else if(arr[i].length > 1){
+        arr.splice(i,1, parseFloat(arr[i][0]), parseFloat(arr[i][1]));
+      }
     }
+  }
+      for(let z in arr){
+        if(Array.isArray(arr[z])){
+          arr[z] = arr[z][0][0]; 
+        }
+      }
+      return arr; 
 }
 
-    return newarr; 
-}
-
+console.log(steamrollArray([1, {}, [3, [[4]]]]));
 console.log(steamrollArray([1, [2], [3, [[4]]]]));
 console.log(steamrollArray([[["a"]], [["b"]]]));
 console.log(steamrollArray([1, [], [3, [[4]]]]));
-console.log(steamrollArray([1, {}, [3, [[4]]]])); 
-
-
-
-
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
 
 
 
